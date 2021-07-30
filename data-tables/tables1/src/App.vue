d3ba57162b079bba2e653f597e415b0e702426e0
BikoCodes <bikocodes@gmail.com>
2021-07-30 23:33:09 +0300

    vue-dataTables

---
+++ data-tables/tables1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container y:margin-5">
+    <DataTable></DataTable>
+  </div>
+</template>
+
+<script >
+import DataTable from './components/DataTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DataTable
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
